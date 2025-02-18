19b047a25602c9a83485f355143ae821b9b63b16
Tarani Kondoori <ktara@MacBook-Pro-46.attlocal.net>
2025-02-17 16:30:11 -0800

    First commit

---
+++ src/sections/Testimonials.tsx
@@ -0,0 +1,88 @@
+import memojiAvatar1 from "@/assets/images/memoji-avatar-1.png";
+import memojiAvatar2 from "@/assets/images/memoji-avatar-2.png";
+import memojiAvatar3 from "@/assets/images/memoji-avatar-3.png";
+import memojiAvatar4 from "@/assets/images/memoji-avatar-4.png";
+import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
+import { SectionHeader } from "@/components/SectionHeader";
+import Image from "next/image";
+import { Card } from "@/components/Card";
+import { Fragment } from "react";
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
+const testimonials = [
+  {
+    name: "Alex Turner",
+    position: "Marketing Manager @ TechStartups",
+    text: "Alex was instrumental in transforming our website into a powerful marketing tool. His attention to detail and ability to understand our brand is exceptional. We're thrilled with the results!",
+    avatar: memojiAvatar1,
+    link: "#",
+  },
+  {
+    name: "Olivia Green",
+    position: "Head of Design @ GreenLeaf",
+    text: "Working with Alex was a pleasure. His expertise in frontend development brought our designs to life in a way we never imagined. The website has exceeded our expectations.",
+    avatar: memojiAvatar2,
+    link: "#",
+  },
+  {
+    name: "Daniel White",
+    position: "CEO @ InnovateCo",
+    text: "Alex's ability to create seamless user experiences is unmatched. Our website has seen a significant increase in conversions since launching the new design. We couldn't be happier.",
+    avatar: memojiAvatar3,
+    link: "#",
+  },
+  {
+    name: "Emily Carter",
+    position: "Product Manager @ GlobalTech",
+    text: "Alex is a true frontend wizard. He took our complex product and transformed it into an intuitive and engaging user interface. We're already seeing positive feedback from our customers.",
+    avatar: memojiAvatar4,
+    link: "#",
+  },
+  {
+    name: "Michael Brown",
+    position: "Director of IT @ MegaCorp",
+    text: "Alex's work on our website has been nothing short of exceptional. He's a talented developer who is also a great communicator. We highly recommend him.",
+    avatar: memojiAvatar5,
+    link: "#",
+  },
+];
+
+export const TestimonialsSection = () => {
+  return <div className="py-16 lg:py-24">
+    <div className="container">
+      <SectionHeader
+        eyebrow="My Learnings"
+        title="Projects"
+        description="From theory to practice—projects that sharpened my skills and sparked innovation!" />
+
+      <div className="mt-12 lg:mt-20 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] py-4 -my-4">
+        <div className="flex flex-none gap-8 animate-move-left [animation-duration:120s] hover:[animation-play-state:paused]">
+          {[...new Array(2)].fill(0).map((_, index) => (
+            <Fragment key={index}>
+              {testimonials.map(testimonial => (
+                <Card key={testimonial.name} className="max-w-xs p-6 md:p-8 md:max-w-md hover:-rotate-3 transition duration-300">
+                  <div className="flex gap-4 items-center">
+                    <div className="size-14 bg-gray-700 inline-flex items-center justify-center rounded-full flex-shrink-0">
+                      <Image src={testimonial.avatar} alt={testimonial.name} className="max-h-full" />
+                    </div>
+                    <div>
+                      <div className="font-semibold">{testimonial.name}</div>
+                      <div className="text-sm text-white/40">{testimonial.position}</div>
+                    </div>
+                  </div>
+                  <p className="mt-4 md:mt-6 text-sm md:text-base">{testimonial.text}</p>
+                  <a href={testimonial.link}>
+                    <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                      <span>View in Github</span>
+                      <ArrowUpRightIcon className="size-4" />
+                    </button>
+                  </a>
+                </Card>
+              ))}
+            </Fragment>
+          ))}
+
+        </div>
+      </div>
+    </div>
+  </div>;
+};
